74c540d6bb07492afe15441a5db24019bb48e518
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2025-04-25 06:23:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 window.addEventListener("DOMContentLoaded", function () {
-    const nameElement = document.getElementById("name");
+  // 타이핑 애니메이션
+  const nameElement = document.getElementById("name");
+  if (nameElement) {
     const text = nameElement.textContent;
-    nameElement.textContent = ""; // 초기화
-  
+    nameElement.textContent = "";
     nameElement.classList.add("typing");
-  
-    // 한 글자씩 span으로 감싸고 딜레이 줌
     for (let i = 0; i < text.length; i++) {
       const span = document.createElement("span");
       span.textContent = text[i];
       span.style.animationDelay = `${i * 0.1}s`;
       nameElement.appendChild(span);
     }
-  
-    // portfolio는 이제 바로 보이므로 JS에서 제어할 필요 없음!
-  });
+  }
 
-  document.querySelectorAll(".donut-bar").forEach((bar, idx) => {
-    const percent = 80; // 바꾸고 싶은 퍼센트로 설정
-    bar.setAttribute("stroke-dasharray", `${percent} 100`);
+  const fadeImages = document.querySelectorAll('.fade-img');
+
+const handleScroll = () => {
+  fadeImages.forEach((img) => {
+    const rect = img.getBoundingClientRect();
+    const triggerPoint = window.innerHeight * 0.9;
+
+    if (rect.top < triggerPoint) {
+      img.classList.add('fade-in');
+    }
   });
+};
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('DOMContentLoaded', handleScroll);
+  
+});
